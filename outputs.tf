743a60d3936dde5a98e16b9c06b505fb79c4b1b2
SESA444562 <sesa444562@se.com>
2024-06-07 12:15:31 +0200

    Add outputs

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,15 @@ output "app_url" {
   value       = "https://${var.destination_org}.github.io/${var.waypoint_application}"
   description = "The URL of the app on GitHub Pages."
 }
+
+output "app_insights_connection_string" {
+  value       = azurerm_application_insights.appi.connection_string
+  description = "The connection string for the created Azure Application Insights instance."
+  sensitive   = true
+}
+
+output "app_insights_key" {
+  value       = azurerm_application_insights.appi.instrumentation_key
+  description = "The instrumentation key for the created Azure Application Insights instance."
+  sensitive   = true
+}
